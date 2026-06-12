b0312a7151dadc7b182f819fde29d7226543d3d5
Basty66 <cri.cerdaa@duocuc.cl>
2026-06-12 00:18:48 -0400

    centralize SITE_URL via VITE_SITE_URL env var, prep for domain change

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -6,6 +6,6 @@ export const WHATSAPP_DEFAULT_MSG = `Hola%20Bastian%2C%20tengo%20una%20consulta%
 
 export const WHATSAPP_FULL = `${WHATSAPP_LINK}?text=${WHATSAPP_DEFAULT_MSG}`;
 
-export const SITE_URL = 'https://frontbastianpage.vercel.app';
+export const SITE_URL = import.meta.env.VITE_SITE_URL || 'https://frontbastianpage.vercel.app';
 
 export const SITE_NAME = 'Bastian.dev';
